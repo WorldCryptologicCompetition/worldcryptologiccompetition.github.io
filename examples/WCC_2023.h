ceeee71e44d9108333559069708b62377572f10d
Administrator <WorldCryptologicCompetition@gmail.com>
2023-04-13 03:54:10 -0400

    Create WCC_2023.h

---
+++ examples/WCC_2023.h
@@ -0,0 +1,22 @@
+
+/*
+ * WCC_2023.h
+ *
+ * Entrants are encouraged to use this library, but it is not a requirement.
+ *
+ */
+
+#define ROR(x, r) ((x >> r) | (x << (64 - r)))
+#define ROL(x, r) ((x << r) | (x >> (64 - r)))
+
+uint64_t ROL64(uint64_t word, const uint8_t rotation)
+{
+    return ((word << rotation) | (word >> (64 - rotation));
+}
+            
+uint64_t ROR64(uint64_t word, const uint8_t rotation)
+{
+    return ((word >> rotation) | (word << (64 - rotation));
+}
+            
+            
