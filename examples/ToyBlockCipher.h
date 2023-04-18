6749ff3f67c43aae5371ef16ab959017d1d80367
Administrator <WorldCryptologicCompetition@gmail.com>
2023-04-18 12:05:23 -0400

    Work on TBC

--- examples/ToyBlockCipher.h
+++ examples/ToyBlockCipher.h
@@ -35,6 +35,15 @@ void entry_TBC_util_reverseKeyIdx(uint_fast8_t * const idx)
 	}
 }
 
+void entry_TBC_util_reverseNonceIdx(uint_fast8_t * const idx)
+{
+	if ((*idx) == 0) {
+		(*idx) = 2;
+	} else {
+		(*idx)--;
+	}
+}
+
 void entry_TBC_runKeySchedule(struct entry_TBC_state * const state)
 {
     static const uint8_t rotations[5][2] = {
@@ -73,20 +82,24 @@ void entry_TBC_ENC(struct entry_TBC_state * const state)
 
     entry_TBC_runKeySchedule(state);
 
-    nonceWordIdx = 0;
+    
     for (blockWordIdx = 0; blockWordIdx < 8; blockWordIdx++) {
-        state->ciphertext[blockWordIdx] = state->plaintext[blockWordIdx] ^ state->nonce[nonceWordIdx] ^ blockInitializers[blockWordIdx];
-        nonceWordIdx++; nonceWordIdx %= 3;
+        state->ciphertext[blockWordIdx] = state->plaintext[blockWordIdx] ^ blockInitializers[blockWordIdx];
     }
 
+    nonceWordIdx = 0;
     keyWordIdx = 0;
     for (roundIdx = 0; roundIdx < ENTRY_TBC_NUMBER_OF_ROUNDS; roundIdx++) {
         for (blockWordIdx = 0; blockWordIdx < 8; blockWordIdx += 2) {
-            state->ciphertext[blockWordIdx] += state->ciphertext[blockWordIdx + 1] + state->keySchedule[keyWordIdx]; keyWordIdx++; keyWordIdx %= 13;
+            state->ciphertext[blockWordIdx] += state->ciphertext[blockWordIdx + 1];
+            state->ciphertext[blockWordIdx] += state->nonce[nonceWordIdx]; nonceWordIdx++; nonceWordIdx %= 3;
+            state->ciphertext[blockWordIdx] += state->keySchedule[keyWordIdx]; keyWordIdx++; keyWordIdx %= 13;
             state->ciphertext[blockWordIdx]  = ROL64(state->ciphertext[blockWordIdx], rotations[blockWordIdx]);
         }
         for (blockWordIdx = 1; blockWordIdx < 8; blockWordIdx += 2) {
-            state->ciphertext[blockWordIdx] += state->ciphertext[blockWordIdx + 1] + state->keySchedule[keyWordIdx]; keyWordIdx++; keyWordIdx %= 13;
+            state->ciphertext[blockWordIdx] += state->ciphertext[blockWordIdx + 1];
+            state->ciphertext[blockWordIdx] += state->nonce[nonceWordIdx]; nonceWordIdx++; nonceWordIdx %= 3;
+            state->ciphertext[blockWordIdx] += state->keySchedule[keyWordIdx]; keyWordIdx++; keyWordIdx %= 13;
             state->ciphertext[blockWordIdx]  = ROL64(state->ciphertext[blockWordIdx], rotations[blockWordIdx]);
         }
     }
@@ -95,7 +108,7 @@ void entry_TBC_ENC(struct entry_TBC_state * const state)
         state->ciphertext[blockWordIdx] ^= state->key[blockWordIdx];
     }
 
-    puts("DEBUG: Ciphertext:");
+    puts("DEBUG: ToyBlockCipher.h: Ciphertext:");
     for (blockWordIdx = 0; blockWordIdx < 8; blockWordIdx++) {
         printf(" 0x%.16" PRIx64, state->ciphertext[blockWordIdx]);
     }
@@ -123,40 +136,47 @@ void entry_TBC_DEC(struct entry_TBC_state * const state)
         state->plaintext[blockWordIdx] = state->ciphertext[blockWordIdx] ^ state->key[blockWordIdx];
     }
 
+    nonceWordIdx = ((ENTRY_TBC_NUMBER_OF_ROUNDS * 8) - 1) % 3;
     keyWordIdx = ((ENTRY_TBC_NUMBER_OF_ROUNDS * 8) - 1) % 13;
     for (roundIdx = 0; roundIdx < ENTRY_TBC_NUMBER_OF_ROUNDS; roundIdx++) {
 	    state->plaintext[7]  = ROR64(state->plaintext[7], rotations[7]);
 	    state->plaintext[7] -= (state->plaintext[0] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
+        state->plaintext[7] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
 
 	    state->plaintext[5]  = ROR64(state->plaintext[5], rotations[5]);
 	    state->plaintext[5] -= (state->plaintext[6] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
+        state->plaintext[5] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
 
 	    state->plaintext[3]  = ROR64(state->plaintext[3], rotations[3]);
 	    state->plaintext[3] -= (state->plaintext[4] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
+        state->plaintext[3] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
 
 	    state->plaintext[1]  = ROR64(state->plaintext[1], rotations[1]);
 	    state->plaintext[1] -= (state->plaintext[2] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
-
+        state->plaintext[1] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
 
 	    state->plaintext[6]  = ROR64(state->plaintext[6], rotations[6]);
 	    state->plaintext[6] -= (state->plaintext[7] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
+        state->plaintext[6] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
 
 	    state->plaintext[4]  = ROR64(state->plaintext[4], rotations[4]);
 	    state->plaintext[4] -= (state->plaintext[5] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
+        state->plaintext[4] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
 
 	    state->plaintext[2]  = ROR64(state->plaintext[2], rotations[2]);
 	    state->plaintext[2] -= (state->plaintext[3] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
+        state->plaintext[2] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
 
 	    state->plaintext[0]  = ROR64(state->plaintext[0], rotations[0]);
 	    state->plaintext[0] -= (state->plaintext[1] + state->keySchedule[keyWordIdx]); entry_TBC_util_reverseKeyIdx(&keyWordIdx);
+        state->plaintext[0] -= state->nonce[nonceWordIdx]; entry_TBC_util_reverseNonceIdx(&nonceWordIdx);
     }
 
     for (blockWordIdx = 0; blockWordIdx < 8; blockWordIdx++) {
-        state->plaintext[blockWordIdx] ^= state->nonce[nonceWordIdx] ^ blockInitializers[blockWordIdx];
-        nonceWordIdx++; nonceWordIdx %= 3;
+        state->plaintext[blockWordIdx] ^= blockInitializers[blockWordIdx];
     }
 
-    puts("DEBUG: Plaintext:");
+    puts("DEBUG: ToyBlockCipher.h: Plaintext:");
     for (blockWordIdx = 0; blockWordIdx < 8; blockWordIdx++) {
         printf(" 0x%.16" PRIx64, state->plaintext[blockWordIdx]);
     }
